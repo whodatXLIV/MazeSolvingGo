8ae6dc2d6d55302b80f1c9349b02ef8144820d03
whodatXLIV <sethamin.7@gmail.com>
2018-03-13 23:50:42 -0700

    improving maze extra

--- maze/manipulate.go
+++ maze/manipulate.go
@@ -7,28 +7,29 @@ import (
 )
 
 func PrepareMaze(img image.Image) (map[int][]int, error) {
-	oldImg := img.(*image.Paletted)
+	maze := img.(*image.Paletted)
 
-	w, h := oldImg.Rect.Dx(), oldImg.Rect.Dy()
-
+	w, h := maze.Rect.Dx(), maze.Rect.Dy()
+	c := maze.Pix
 	m := make(map[int][]int)
 	var (
 		indx, up, down, left, right int
 		connected, direction        []int
 	)
-	for i := 0; i < w; i++ {
-		for j := 0; j < h; j++ {
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
 			indx = i*w + j
-			if oldImg.Pix[indx] != 0 {
+			if c[indx] != 0 {
 				connected = []int{}
 				up = (i-1)*w + j
 				left = i*w + j - 1
 				right = i*w + j + 1
 				down = (i+1)*w + j
 				direction = []int{up, left, right, down}
+
 				for _, v := range direction {
 					if v >= 0 && v < h*w {
-						if oldImg.Pix[v] != 0 {
+						if c[v] != 0 {
 							connected = append(connected, v)
 						}
 					}
@@ -38,19 +39,60 @@ func PrepareMaze(img image.Image) (map[int][]int, error) {
 			}
 		}
 	}
-	oldImg.Palette = append(oldImg.Palette, color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 0, 255}, color.RGBA{0, 0, 255, 255})
+	maze.Palette = append(maze.Palette, color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 0, 255}, color.RGBA{0, 0, 255, 255})
 	return m, nil
 }
 
+func PrepareMazeExtra(img image.Image) (map[int][]int, error) {
+	maze := img.(*image.Paletted)
+
+	w, h := maze.Rect.Dx(), maze.Rect.Dy()
+	c := maze.Pix
+	m := make(map[int][]int)
+	mt := make(map[int]int)
+	var (
+		indx, up, down, left, right int
+		connected                   int
+	)
+	for j := 0; j < h; j++ {
+		for i := 0; i < w; i++ {
+			indx = i*w + j
+			if c[indx] != 0 {
+				connected = 0
+				up = int(c[(i-1)*w+j])
+				left = int(c[i*w+j-1])
+				right = int(c[i*w+j+1])
+				down = int(c[(i+1)*w+j])
+
+				if left == 1 {
+					if right == 1 {
+						if up == 1 || down == 1 {
+							connected = indx
+
+						}
+					} else {
+						connected = indx
+					}
+
+				}
+
+				m[indx] = connected
+
+			}
+		}
+	}
+	maze.Palette = append(maze.Palette, color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 0, 255}, color.RGBA{0, 0, 255, 255})
+	return m, nil
+	// Think about using struct to hold begining end of maze
+}
 func FindEntrance(img image.Image) (int, error) {
 	b := img.Bounds()
 	w := b.Max.X
 	var p int
-	var rp uint8
+	c := img.(*image.Paletted).Pix
 
 	for i := 0; i < w; i++ {
-		rp = img.(*image.Paletted).Pix[i]
-		if rp > 0 {
+		if c[i] > 0 {
 			p = i
 			return p, nil
 		}
@@ -64,11 +106,11 @@ func FindExit(img image.Image) (int, error) {
 	b := img.Bounds()
 	w, h := b.Max.X, b.Max.Y
 	var p, indx int
-	var rp uint8
+	c := img.(*image.Paletted).Pix
+
 	for i := 0; i < w; i++ {
 		indx = h*(w-1) - 1 + i
-		rp = img.(*image.Paletted).Pix[indx]
-		if rp > 0 {
+		if c[indx] > 0 {
 			p = indx
 			return p, nil
 		}
@@ -79,16 +121,17 @@ func FindExit(img image.Image) (int, error) {
 }
 
 func SolvedColor(img image.Image, ent, ext int, sol []int) {
-	nImg := img.(*image.Paletted)
+	// Palette colors [Black white red green blue]
+	maze := img.(*image.Paletted)
 
 	for _, v := range sol {
-		nImg.Pix[v] = 4
+		maze.Pix[v] = 4
 
 	}
 
-	nImg.Pix[ent] = 3
+	maze.Pix[ent] = 3
 
-	nImg.Pix[ext] = 2
+	maze.Pix[ext] = 2
 
 }
 
